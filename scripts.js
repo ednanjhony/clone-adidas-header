8618497fd46aef4cc1b43efc0ff9204d295275fd
Ednan Jhony <ednanjhony@yahoo.com.br>
2021-02-19 08:07:42 -0300

    Added some js and editing...

---
+++ scripts.js
@@ -0,0 +1,14 @@
+const addDropList = {
+    open() {
+        document
+            .querySelector('.content-list')
+            .classList
+            .add('active')
+    },
+    close() {
+        document
+            .querySelector('.content-list')
+            .classList
+            .remove('active')
+    }
+}
